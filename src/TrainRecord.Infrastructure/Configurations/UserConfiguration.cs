322b8af3b893f02b97f86b178f67a96ada2b2813
Gustavo Messias <Gustavo.Messias@btgpactual.com>
2023-01-05 09:42:08 -0300

    fix: 2 primary key to 1 unique index email user

--- src/TrainRecord.Infrastructure/Configurations/UserConfiguration.cs
+++ src/TrainRecord.Infrastructure/Configurations/UserConfiguration.cs
@@ -13,7 +13,7 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.HasKey(u => u.Id);
-        builder.HasKey(u => u.Email);
+        builder.HasIndex(u => u.Email).IsUnique();
 
         builder.Property(a => a.CreatedAt).HasColumnType("DATETIME");
         builder.Property(a => a.LastModifiedAt).HasColumnType("DATETIME");
