af77cf20b56a4d43d6f664b33773eacd9c010f3e
Gustavo Messias <Gustavo.Messias@btgpactual.com>
2023-01-05 23:14:06 -0300

    feat: add interception to database to add creater and modifier

---
+++ src/TrainRecord.Infrastructure/Interceptions/AuditableEntitySaveChangesInterceptor.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using TrainRecord.Core.Commum;
+using TrainRecord.Infrastructure.Extentions;
+using TrainRecord.Infrastructure.Interfaces;
+
+namespace TrainRecord.Infrastructure.Interceptions
+{
+    public class AuditableEntitySaveChangesInterceptor : SaveChangesInterceptor
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public AuditableEntitySaveChangesInterceptor(ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+        }
+
+        public override InterceptionResult<int> SavingChanges(
+            DbContextEventData eventData,
+            InterceptionResult<int> result
+        )
+        {
+            UpdateEntities(eventData.Context);
+
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+            DbContextEventData eventData,
+            InterceptionResult<int> result,
+            CancellationToken cancellationToken = default
+        )
+        {
+            UpdateEntities(eventData.Context);
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        public void UpdateEntities(DbContext? context)
+        {
+            if (context == null)
+                return;
+
+            foreach (var entry in context.ChangeTracker.Entries<BaseAuditableEntity>())
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedBy = _currentUserService.UserId;
+                    entry.Entity.CreatedAt = DateTime.Now;
+                }
+
+                if (
+                    entry.State == EntityState.Added
+                    || entry.State == EntityState.Modified
+                    || entry.HasChangedOwnedEntities()
+                )
+                {
+                    entry.Entity.LastModifiedBy = _currentUserService.UserId;
+                    entry.Entity.LastModifiedAt = DateTime.Now;
+                }
+            }
+        }
+    }
+}
