79ae183d409eff55da778489205f1f6f3a85b432
Gustavo Messias <Gustavo.Messias@btgpactual.com>
2023-02-03 22:48:14 -0300

    feat: get time to each request

---
+++ src/TrainRecord.Api/Middlewares/LogMiddleware.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using System.Text;
+using TrainRecord.Core.Extentions;
+
+namespace TrainRecord.Api.Middlewares
+{
+    public class LogMiddleware
+    {
+        private readonly Stopwatch _timer;
+        private readonly RequestDelegate _next;
+        private readonly ILogger<LogMiddleware> _logger;
+
+        public LogMiddleware(RequestDelegate next, ILogger<LogMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+            _timer = new Stopwatch();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            var path = request.Path;
+            var statusCode = response.StatusCode;
+
+            var elapsed = await _timer.GetTimeAsync(() => _next(context));
+
+            request.Body.Position = 0;
+            var bodyContent = await new StreamReader(request.Body).ReadToEndAsync();
+
+            _logger.LogInformation(
+                "TrainRecord Long Running: {path} BodyContent: {bodyContent} Elapsed: {elapsed} StatusCode: {statusCode}",
+                path,
+                bodyContent,
+                elapsed,
+                statusCode
+            );
+        }
+    }
+}
