3b40541880903da4183d5882e9e25edda1a2fada
Gustavo Messias <gustavo.hmessias96@gmail.com>
2023-02-15 20:00:51 -0300

    fix: pagination return items

--- src/TrainRecord.Core/Extentions/QueryableExtention.cs
+++ src/TrainRecord.Core/Extentions/QueryableExtention.cs
@@ -31,13 +31,13 @@ namespace TrainRecord.Core.Extentions
             Pagination pagination
         )
         {
-            if (pagination is null)
+            if (pagination.IsNotRequestedPage())
             {
                 return queryable;
             }
 
-            var perPage = pagination.PerPage;
-            var pageNumber = pagination.PageNumber;
+            var perPage = pagination.PerPage!.Value;
+            var pageNumber = pagination.PageNumber!.Value;
 
             var pageToSkip = (pageNumber - 1) * perPage;
             return queryable.Skip(pageToSkip).Take(perPage);
