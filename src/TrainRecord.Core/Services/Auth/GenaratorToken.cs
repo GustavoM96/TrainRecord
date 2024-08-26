ed46660a4949e7a6f39a846e4e8d2a6c5eb76fd7
Gustavo Messias <gustavo.messias96@gmail.com>
2024-08-26 20:42:05 -0300

    feat: add authentication token

--- src/TrainRecord.Core/Services/Auth/GenaratorToken.cs
+++ src/TrainRecord.Core/Services/Auth/GenaratorToken.cs
@@ -6,41 +6,41 @@ using Microsoft.IdentityModel.Tokens;
 using TrainRecord.Core.Entities;
 using TrainRecord.Core.Interfaces;
 
-namespace TrainRecord.Core.Services.Auth
+namespace TrainRecord.Core.Services.Auth;
+
+public class GenaratorToken : IGenaratorToken
 {
-    public class GenaratorToken : IGenaratorToken
+    private readonly string _secretKey;
+
+    public GenaratorToken(IConfiguration configuration)
     {
-        private readonly string _secretKey;
+        _secretKey = configuration.GetSection("Jwt").GetSection("SecretKey").Value!;
+    }
 
-        public GenaratorToken(IConfiguration configuration)
+    public ApiTokenResponse Generate(User user)
+    {
+        var key = Encoding.ASCII.GetBytes(_secretKey);
+        var apiTokenResponse = new ApiTokenResponse(4);
+        var tokenDescriptor = new SecurityTokenDescriptor()
         {
-            _secretKey = configuration.GetSection("Jwt").GetSection("SecretKey").Value!;
-        }
+            Subject = new ClaimsIdentity(
+                new Claim[]
+                {
+                    new Claim(ClaimTypes.Sid, user.Id.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Role, user.Role.ToString()),
+                }
+            ),
+            Expires = apiTokenResponse.ExpiresDateTime,
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature
+            ),
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
 
-        public string Generate(User user)
-        {
-            var key = Encoding.ASCII.GetBytes(_secretKey);
-            var tokenDescriptor = new SecurityTokenDescriptor()
-            {
-                Subject = new ClaimsIdentity(
-                    new Claim[]
-                    {
-                        new Claim(ClaimTypes.Sid, user.Id.ToString()),
-                        new Claim(ClaimTypes.Email, user.Email),
-                        new Claim(ClaimTypes.Role, user.Role.ToString()),
-                    }
-                ),
-                Expires = DateTime.UtcNow.AddHours(100),
-                SigningCredentials = new SigningCredentials(
-                    new SymmetricSecurityKey(key),
-                    SecurityAlgorithms.HmacSha256Signature
-                ),
-            };
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-
-            return tokenHandler.WriteToken(token);
-        }
+        apiTokenResponse.Key = tokenHandler.WriteToken(securityToken);
+        return apiTokenResponse;
     }
 }
