7ff838fa29e7087aa5f7bc4671b46830cbac0298
Gustavo Henrique Messias <gustavomessias96@outlook.com>
2024-09-04 10:59:54 -0300

    fix: response informations

--- src/TrainRecord.Api/Common/Controllers/ApiResponse.cs
+++ src/TrainRecord.Api/Common/Controllers/ApiResponse.cs
@@ -2,12 +2,12 @@ namespace TrainRecord.Api.Common.Controllers;
 
 public record ApiOkResponse(object Data, string TraceId)
 {
-    public DateTime DateTime = DateTime.Now;
-    public int StatusCode = 200;
+    public DateTime DateTime { get; set; } = DateTime.Now;
+    public int StatusCode { get; set; } = 200;
 }
 
 public record ApiCreatedResponse(object Data, string TraceId)
 {
-    public DateTime DateTime = DateTime.Now;
-    public int StatusCode = 201;
+    public DateTime DateTime { get; set; } = DateTime.Now;
+    public int StatusCode { get; set; } = 201;
 }
